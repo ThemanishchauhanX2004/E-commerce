becbac9078139cd3492cf7aa2a4c2b7c1af6505a
manish chauhan <manishchauhan7783@gmail.com>
2025-11-12 17:50:32 +0530

    first-commit

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: "dnyoqiz7b",
+  api_key: "322762245933415",
+  api_secret: "CkOA5QMakA65oHu9HGGXmV2x-qU"
+});
+
+export default cloudinary;
